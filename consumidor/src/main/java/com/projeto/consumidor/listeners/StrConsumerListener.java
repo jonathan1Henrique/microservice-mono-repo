146cdb23a319517c9b4605f9702fbbdd55e9675a
Jonathan1Henrique <jonathan1_henrique@hotmail.com>
2022-12-27 20:57:38 -0300

    :tada: custom listener

--- consumidor/src/main/java/com/projeto/consumidor/listeners/StrConsumerListener.java
+++ consumidor/src/main/java/com/projeto/consumidor/listeners/StrConsumerListener.java
@@ -1,18 +1,30 @@
 package com.projeto.consumidor.listeners;
 
+import com.projeto.consumidor.custom.StrConsumerCustomListener;
 import lombok.extern.log4j.Log4j2;
-import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.annotation.TopicPartition;
 import org.springframework.stereotype.Component;
 
 @Log4j2
 @Component
 public class StrConsumerListener {
 
-    @KafkaListener(groupId = "group-1",
-            topics = "str-topic",
-            containerFactory = "strContainerFactory"
+    @StrConsumerCustomListener(groupId = "group-1")
+    public void create(String message) {
+        log.info(" CREATE ::: Receive message {}", message);
+    }
+
+    @StrConsumerCustomListener(groupId = "group-1")
+    public void log(String message) {
+        log.info("LOG ::: Receive message {}", message);
+    }
+
+    @StrConsumerCustomListener(groupId = "group-2",
+            topicPartitions = {
+                    @TopicPartition(topic = "str-topic", partitions = {"1"}),
+            }
     )
-    public void create(String message){
-       log.info("Receive message {}", message);
+    public void history(String message) {
+        log.info(" HISTORY ::: Receive message {}", message);
     }
 }
